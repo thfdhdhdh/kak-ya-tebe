4e516752c7cd3c66a29b89b9ab3203b24a5a7c62
GhostRichards <theghostrichards@gmail.com>
2025-12-02 17:55:13 +0500

    Remove unnecessary files

--- deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Деплой сайта на Surge.sh...
-echo.
-cd /d %~dp0
-surge . kak-ya-tebe.surge.sh
-echo.
-echo Готово! Если деплой прошел успешно, ваш сайт доступен по адресу:
-echo https://kak-ya-tebe.surge.sh
-pause
